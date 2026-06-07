de06c7bc7cd56130fda982343f215ae3d88bcff6
mrask31 <admin@mjrintelligencegroup.com>
2026-06-07 08:53:22 -0500

    fix(analytics): wire minimal PostHog provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import Script from "next/script";
 import "./globals.css";
+import { PHProvider } from "@/components/providers/posthog-provider";
 
 const dmSans = DM_Sans({
   subsets: ["latin"],
@@ -29,7 +30,7 @@ export default function RootLayout({
             __html: `
               (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+dl:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
               })(window,document,'script','dataLayer','GTM-WGRTRVG9');
             `,
@@ -43,7 +44,7 @@ export default function RootLayout({
             style={{ display: "none", visibility: "hidden" }}
           />
         </noscript>
-        {children}
+        <PHProvider>{children}</PHProvider>
       </body>
     </html>
   );
